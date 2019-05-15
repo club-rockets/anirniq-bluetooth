c2b2ec1627c9b8aff6a5e5ea0d64cf4c13d38c6c
Marc-André Denis <marc-andre.denis.3@ens.etsmtl.ca>
2019-05-15 11:29:24 -0400

    Fixed advertising packet too large

--- Debug/source/mtb.d
+++
@@ -1,4 +0,0 @@
-source/mtb.o source/mtb.d: ../source/mtb.c \
- C:/Users/Marc-Andre/Desktop/frdmkw41z_wireless_examples_bluetooth_heart_rate_sensor_freertos_aa/source/app_preinclude.h
-
-C:/Users/Marc-Andre/Desktop/frdmkw41z_wireless_examples_bluetooth_heart_rate_sensor_freertos_aa/source/app_preinclude.h:
--- source/app_config.c
+++ source/app_config.c
@@ -65,8 +65,8 @@ static const gapAdStructure_t advScanStruct[3] = {
   },  
   {
     .adType = gAdShortenedLocalName_c,
-    .length = 10,
-    .aData = (uint8_t*)"ANIRNIQ_1"
+    .length = 9,
+    .aData = (uint8_t*)"ANIRNIQ1"
   }  
 };
 
